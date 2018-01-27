9be2beecf6ec583f150724b8c06c4f144ae57838
y.matsumura <y.matsumura@raksul.com>
2018-01-27 10:28:10 +0900

    fix spec

--- spec/lib/dollar_spec.rb
+++ spec/lib/dollar_spec.rb
@@ -4,10 +4,11 @@ require 'dollar'
 describe 'MoneyTest' do
   it 'multiplication' do
     five = Dollar.new(5)
-    five.times(2)
-    expect(five.amount).to eq 10
 
-    five.times(3)
-    expect(five.amount).to eq 15
+    product = five.times(2)
+    expect(product.amount).to eq 10
+
+    product = five.times(3)
+    expect(product.amount).to eq 15
   end
 end
