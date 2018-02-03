271c649a9b7eebc25c6345ed5f48de92a5d34041
y.matsumura <y.matsumura@raksul.com>
2018-02-03 12:05:40 +0900

    refactor spec

--- spec/lib/dollar_spec.rb
+++ spec/lib/dollar_spec.rb
@@ -5,12 +5,12 @@ describe Dollar do
   describe '#times' do
     let(:five) { Money.dollar(5) }
 
-    it { expect(five.times(2)) == Dollar.new(10) }
-    it { expect(five.times(3)) == Dollar.new(15) }
+    it { expect(five.times(2)) == Money.dollar(10) }
+    it { expect(five.times(3)) == Money.dollar(15) }
   end
 
   describe '#equals' do
-    it { expect(Dollar.new(5).equals(Dollar.new(5))).to eq true }
-    it { expect(Dollar.new(5).equals(Dollar.new(6))).to eq false }
+    it { expect(Money.dollar(5).equals(Money.dollar(5))).to eq true }
+    it { expect(Money.dollar(5).equals(Money.dollar(6))).to eq false }
   end
 end
