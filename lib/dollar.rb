710057ebc253f8dd77198ac951b8fa33195abb08
y.matsumura <y.matsumura@raksul.com>
2018-02-03 10:34:12 +0900

    move getter

--- lib/dollar.rb
+++ lib/dollar.rb
@@ -1,9 +1,6 @@
 require 'money'
 
 class Dollar < Money
-  # TODO: Rubyでのインスタンス変数のprivate化がわからないので一旦保留
-  attr_accessor :amount
-
   def initialize(amount)
     @amount = amount
   end
--- lib/money.rb
+++ lib/money.rb
@@ -1,2 +1,4 @@
 class Money
+  # TODO: Rubyでのインスタンス変数のprivate化がわからないので一旦保留
+  attr_accessor :amount
 end
